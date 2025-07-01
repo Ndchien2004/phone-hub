e74ca02acabcafba719e76990cf602a21a1007f4
U2Ber202 <duonghaiduc1412@gmail.com>
2025-07-02 01:51:10 +0700

    Adding DateUtil from origin/dev and refractor location package
    Adding DateUtil from origin/dev for productDAO and refractor location package of Service and Util cause of mistake when created

--- src/main/java/model/service/impl/HomeServiceImpl.java
+++
@@ -1,7 +0,0 @@
-package model.service.impl;
-
-import model.service.HomeService;
-
-public class HomeServiceImpl implements HomeService {
-    
-}
---
+++ src/main/java/service/impl/HomeServiceImpl.java
@@ -0,0 +1,12 @@
+package service.impl;
+
+import model.dao.ProductDAO;
+import model.dao.impl.ProductDAOImpl;
+import service.HomeService;
+
+public class HomeServiceImpl implements HomeService {
+
+    private final ProductDAO productDAO = new ProductDAOImpl();
+
+    public
+}
